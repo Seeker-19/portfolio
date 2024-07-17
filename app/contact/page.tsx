e98bc0b9a5dab25e57398bfca9e93ddc4bf35412
Anurag <kanswalanurag123@gmail.com>
2024-07-17 23:28:39 +0530

    done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -50,11 +50,11 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-9 bg-slate-200 rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-black/70">
                 Feel free to reach out to me through any of the following
-                methods. I'm always open to discussing new projects, creative
-                ideas, or opportunities to be part of your vision.
+                methods. I&apos;m always open to discussing new projects,
+                creative ideas, or opportunities to be part of your vision.
               </p>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
